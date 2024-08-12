6db0b9c682896d36eb3af0aea16ba11ef5709b70
Robiul Islam <therobiulislam12@gmail.com>
2024-08-13 01:49:09 +0600

    Added Sortable column

--- admin/Menu.php
+++ admin/Menu.php
@@ -6,10 +6,31 @@
 class Menu {
     public function __construct() {
 
+        // add column
         add_filter( 'manage_r_simple_slider_posts_columns', array( $this, 'ss_thumbnail_column' ), 10, 1 );
 
+        // add column data
         add_filter( 'manage_r_simple_slider_posts_custom_column', array( $this, 'ss_thumbnail_show' ), 10, 2 );
 
+        // make column sortable
+        add_filter( 'manage_edit-r_simple_slider_sortable_columns', array( $this, 'ss_custom_column_sortable' ), 10, 1 );
+
+    }
+
+
+    /**
+     * Make sortable columns
+     * 
+     * @param array $sortable_columns
+     * 
+     * @return array
+     */
+    public function ss_custom_column_sortable($sortable_columns){
+        
+        $sortable_columns['slider_serial'] = 'slider_serial';
+
+
+        return $sortable_columns;
     }
 
     public function ss_thumbnail_column($columns){
